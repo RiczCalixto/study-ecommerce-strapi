9029c58296b23ecaef553eab4698b3765ba69a21
RiczCalixto <rcalixtos@gmail.com>
2020-12-11 18:41:14 -0300

    feat: add new route and controller - populate

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: (ctx) => {
+    console.log("Populating games");
+    ctx.send({ ok: true });
+  },
+};
