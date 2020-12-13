c82021d637ab48ee533ed724967f673f96ecf318
RiczCalixto <rcalixtos@gmail.com>
2020-12-13 17:52:13 -0300

    feat: add crawler to populate strapi

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -6,8 +6,11 @@
  */
 
 module.exports = {
-  populate: (ctx) => {
-    console.log("Populating games");
-    ctx.send({ ok: true });
+  populate: async (ctx) => {
+    console.log("Starting to populate....");
+
+    await strapi.services.game.populate();
+
+    console.log("Finished populating!");
   },
 };
